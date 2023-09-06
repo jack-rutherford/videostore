65619427d89c45f1a8adccc9ec4362942fef33e9
Jack Rutherford <jack.rutherford@hope.edu>
2023-09-06 13:31:18 -0400

    Initial version of solution and Model Tests

---
+++ ModelTest/CustomerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Model;
+
+namespace ModelTest
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Customer customer = new Customer()
+            {
+                Name = new Name()
+                {
+                    First = "Stu",
+                    Last = "Dent",
+                }
+            };
+
+            Assert.IsNotNull(customer.PreferredStores);
+            Assert.AreEqual(0, customer.PreferredStores.Count);
+        }
+    }
+}
---
+++ ModelTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ModelTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ModelTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("982d257e-96f7-446f-b5f8-675a6256347e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
