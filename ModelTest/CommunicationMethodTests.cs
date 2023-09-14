bd3f9db876b821c2762fd688299bdcc61b14a2ef
Jack Rutherford <jack.rutherford@hope.edu>
2023-09-13 22:15:48 -0400

    Implemented CommunicationMethodTests

---
+++ ModelTest/CommunicationMethodTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Model;
+
+namespace ModelTest
+{
+    [TestClass]
+    public class CommunicationMethodTests
+    {
+        public Boolean TestEqualMethod(CommunicationMethod c1, CommunicationMethod c2)
+        {
+            return c1.Name.Equals(c2.Name);
+        }
+        [TestMethod]
+        public void TestEqual()
+        {
+            CommunicationMethod c1 = new CommunicationMethod() { Name = "A" };
+            CommunicationMethod c2 = new CommunicationMethod() { Name = "A" };
+            Assert.AreEqual(TestEqualMethod(c1,c2), c1.Equals(c2));
+        }
+
+        [TestMethod]
+        public void TestGetHashCode()
+        {
+            CommunicationMethod c = new CommunicationMethod() { Name = "A" };
+            Assert.AreEqual(c.GetHashCode(), c.Id);
+        }
+    }
+}
