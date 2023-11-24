014c0614ed1e7610d3a45ef4a9e0aad97a2d6d7a
Jack Rutherford <jack.rutherford@hope.edu>
2023-11-24 13:09:32 -0500

    Added migrations to videostore-team

---
+++ Migrations/M1_Schema.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace Migrations
+{
+    [Migration(1)]
+    public class M1_Schema : Migration
+    {
+        public override void Down()
+        {
+            Delete.Schema("videostore");
+        }
+
+        public override void Up()
+        {
+            Create.Schema("videostore");
+        }
+    }
+}
