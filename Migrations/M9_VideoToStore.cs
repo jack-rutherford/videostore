3a7bb5de36823ac65ad6e113290e4cce6d696452
Jack Rutherford <jack.rutherford@hope.edu>
2023-11-28 12:38:02 -0500

    Migration for Videoto have Store foreign key

---
+++ Migrations/M9_VideoToStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace Migrations
+{
+    [Migration(9)]
+    public class M9_VideoToStore : Migration
+    {
+       public override void Down()
+        {
+            Delete.ForeignKey("VideoToStore").OnTable("Video").InSchema(Names.Schema);
+            Delete.Column("Store_Id").FromTable("Video").InSchema(Names.Schema);
+        }
+
+        public override void Up()
+        {
+            Alter.Table("Video")
+                .InSchema(Names.Schema)
+                .AddColumn("Store_Id").AsInt32().Nullable();
+            Create.ForeignKey("VideoToStore")
+                .FromTable("Video")
+                .InSchema(Names.Schema)
+                .ForeignColumn("Store_Id")
+                .ToTable("Store")
+                .InSchema(Names.Schema)
+                .PrimaryColumn("Id")
+                .OnDelete(System.Data.Rule.SetNull)
+                .OnUpdate(System.Data.Rule.Cascade);
+        }
+    }
+}
