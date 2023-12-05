6c842d5d6266cbb2e6828d41f9f4c8321cea5b51
Jacob Peterson <jacob.peterson@hope.edu>
2023-12-04 20:17:39 -0500

    Added ZipCodeArea and CustomerStore Migrations

---
+++ Migrations/M10_ZipCodeAreaAssociation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace Migrations
+{
+    [Migration(10)]
+    public class M10_ZipCodeAreaAssociation : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("ZipCodeAreaAssociation").InSchema(Names.Schema);
+        }
+
+        public override void Up()
+        {
+            Create.Table("ZipCodeAreaAssociation").InSchema(Names.Schema)
+            .WithColumn("Area_Id").AsInt32().Nullable()
+            .WithColumn("ZipCodes").AsInt32().Nullable()
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity().NotNullable();
+        }
+    }
+}
---
+++ Migrations/M11_CustomerStoreAssociation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace Migrations
+{
+    [Migration(11)]
+    public class M11_CustomerStoreAssociation : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("CustomerStoreAssociation").InSchema(Names.Schema);
+        }
+
+        public override void Up()
+        {
+            Create.Table("CustomerStoreAssociation").InSchema(Names.Schema)
+            .WithColumn("Customer_Id").AsInt32().Nullable()
+            .WithColumn("PreferredStores").AsInt32().Nullable()
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity().NotNullable();
+        }
+    }
+}
